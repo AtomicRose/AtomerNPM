2bef9a3e0bc55bac2923b638996248271b023801
AtomicRose <baishenfang@hotmail.com>
2017-04-13 18:41:55 +0800

    update

--- src/backEnd/config/config.js
+++ src/backEnd/config/config.js
@@ -1,7 +1,8 @@
 let SystemConfig;
 // console.log(process.env);
 SystemConfig = {
-    NPM_PACKAGE_STORAGE: '/home/npm_package_storage'
+    NPM_PACKAGE_STORAGE: 'E://git/AtomerNPM/npm_package_storage'
+    // NPM_PACKAGE_STORAGE: '/home/npm_package_storage'
 }
 
 module.exports = SystemConfig;
--- src/components/PackagesList/PackageInfo.js
+++ src/components/PackagesList/PackageInfo.js
@@ -1,14 +1,23 @@
 import 'SCSS/theme/package-info.scss';
 import React, {Component} from 'react';
+import {history} from 'STORE';
 
 class PackageInfo extends Component {
+    handelGoDetail(packageName) {
+        history.push({
+            pathname: '/' + packageName
+        });
+    }
+
     render() {
         return (
-            <div className="col-xs-12 col-sm-6 col-md-4">
+            <div className="col-xs-12 col-sm-6 col-md-4"
+                 onClick={() => this.handelGoDetail(this.props.packageInfo.name)}>
                 <div className="package-info container-fluid">
                     <div className="col-md-8 package-name">{this.props.packageInfo.name}</div>
                     <div className="col-md-4">
-                        <div className="package-version"><span className="tip">version</span><span className="value">{this.props.packageInfo.version}</span></div>
+                        <div className="package-version"><span className="tip">version</span><span
+                            className="value">{this.props.packageInfo.version}</span></div>
                     </div>
                     <div className="col-md-12 package-description">
                         <textarea value={this.props.packageInfo.description} rows="3" readOnly="readOnly"></textarea>
